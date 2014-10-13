4710ea5aed44bf603f21b1aa5a45588efe9d09e8
Martin Page <wookoouk@gmail.com>
2014-10-13 23:59:47 +0100

    home tests

---
+++ test/Home.js
@@ -0,0 +1,35 @@
+var expect = require('chai').expect;
+var should = require('chai').should();
+var superagent = require('superagent');
+
+describe('ExperimentsController', function() {
+
+// before(function(done){
+//   var App = require('../index').start(done);
+// });
+
+
+
+  describe('get /', function(){
+    it('should work', function(done){
+      superagent.get('http://localhost:8080/')
+      .end(function(e,res){
+        expect(e).to.eql(null);
+        res.status.should.eq(200);
+        done();
+      });
+    });
+  });
+
+  describe('get /us', function(){
+    it('should work',function(done){
+      superagent.get('http://localhost:8080/us')
+      .end(function(e,res){
+        expect(e).to.eql(null);
+        res.status.should.eq(200);
+        done();
+      });
+    });
+  });
+
+});
