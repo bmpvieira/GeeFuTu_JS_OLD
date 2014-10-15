60a9815a7150616c2ed68ac74e544cae007e29a9
Martin Page <wookoouk@gmail.com>
2014-10-15 21:35:02 +0100

    unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,15 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
-// var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-// var jsdoc = require('gulp-jsdoc');
 var rename = require("gulp-rename");
 
-
-// Lint Task
-gulp.task('lint', function () {
-    return gulp.src('public/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
 
 // cp config-example.json -> config.json
 gulp.task('config-file', function () {
     return gulp.src("./config-example.json")
         .pipe(rename("./config.json"))
-        .pipe(gulp.dest("./")); // ./dist/main/text/ciao/goodbye.md
+        .pipe(gulp.dest("./"));
 });
 
 // Compile Our Sass
@@ -34,9 +22,8 @@ gulp.task('sass', function () {
 
 // Watch Files For Changes
 gulp.task('watch', function () {
-    gulp.watch('public/js/*.js', ['lint', 'scripts']);
     gulp.watch('public/scss/*.scss', ['sass']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass']);
+gulp.task('default', 'lint');
