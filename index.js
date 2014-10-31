5164dc4446ede6d4af81695c8cd7f1cfc9fdadf2
Martin Page <wookoouk@gmail.com>
2014-10-31 15:20:26 +0000

    http parse

--- index.js
+++ index.js
@@ -174,7 +174,8 @@ var startApp = function (done) {
 };
 
 var initSocketIO = function(done){
-    var io = require('socket.io')(app);
+    var http = require('http').Server(app);
+    var io = require('socket.io')(http);
     io.on('connection', function(socket){
         console.log('a user connected');
     });
